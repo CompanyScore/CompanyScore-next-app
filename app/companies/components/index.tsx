28bad23c89ea40aa89719d84c1491837374c6ea8
Adil <kemelbekov-adil@mail.ru>
2025-01-27 11:34:50 +0500

    delete: write from index

--- app/companies/components/index.tsx
+++ app/companies/components/index.tsx
@@ -1,5 +1,4 @@
 export { CompaniesFilter } from "./companies-filter";
 export { CompaniesTable } from "./companies-table";
-export { WriteCommentModal } from "./write-comment-modal";
 export { CompaniesCarousel } from "./companies-carousel";
 export { CompaniesText } from "./companies-text";
