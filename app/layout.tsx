93690e644d88a568f71f78522779e4c58ddedeb7
Dimash <dinmukhamed.amirov@gmail.com>
2025-02-05 15:46:30 +0500

    chore: rename

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import ThemeProvider from "@/components/theme-provider";
-import Enter from "@/components/enter";
+import Enter from "@/components/login";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
