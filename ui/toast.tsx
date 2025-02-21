880682d2504409954bf55936543654c895ccdd3b
Adil <kemelbekov-adil@mail.ru>
2025-02-21 14:46:57 +0500

    refactor: update toast

--- ui/toast.tsx
+++ ui/toast.tsx
@@ -1,36 +1,49 @@
 import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import classNames from "classnames";
+import { create } from "zustand";
 
-type ToastProps = {
+type ToastState = {
   message: string | null;
   type?: "success" | "error" | "warning" | "info";
-  className?: string;
+  id?: number;
+  showToast: (
+    message: string,
+    type?: "success" | "error" | "warning" | "info",
+  ) => void;
 };
 
-export function Toast({ message, type = "success", className }: ToastProps) {
-  const [isVisible, setIsVisible] = useState(true);
+export const useToast = create<ToastState>((set) => ({
+  message: null,
+  type: "success",
+  id: 0,
+  showToast: (message, type = "success") =>
+    set({ message, type, id: Date.now() }), // id изменяется для перерисовки
+}));
+
+export function Toast() {
+  const { message, type, id } = useToast();
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    const timer = setTimeout(() => setIsVisible(false), 5000);
-    return () => clearTimeout(timer);
-  }, []);
+    if (message) {
+      setIsVisible(true);
+      const timer = setTimeout(() => setIsVisible(false), 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [id]); // Ререндер при изменении id
 
-  if (!isVisible) return null;
+  if (!isVisible || !message) return null;
 
   return createPortal(
     <div className="toast toast-top toast-end">
       <div
-        className={classNames(
-          "alert text-white",
-          {
-            "alert-success": type === "success",
-            "alert-error": type === "error",
-            "alert-warning": type === "warning",
-            "alert-info": type === "info",
-          },
-          className,
-        )}
+        className={classNames("alert text-white", {
+          "alert-success": type === "success",
+          "alert-error": type === "error",
+          "alert-warning": type === "warning",
+          "alert-info": type === "info",
+        })}
       >
         {message}
       </div>
