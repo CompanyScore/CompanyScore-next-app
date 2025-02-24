58653120ef8912efa7df072b38fc83538c233ed7
Dimash <dinmukhamed.amirov@gmail.com>
2025-02-24 12:51:50 +0500

    feat: protect unauthorized pages

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -7,10 +7,16 @@ import {
   ProfileTable,
 } from "./components";
 import { Title } from "@/ui";
-import { useCommentsStore } from "@/store";
+import { useCommentsStore, useUserStore } from "@/store";
+import { redirect } from "next/navigation";
 
 export default function ProfilePage() {
+  const { userId } = useUserStore();
   const { total } = useCommentsStore();
+
+  if (!userId) {
+    redirect("/");
+  }
 
   return (
     <section className="flex flex-col items-stretch justify-center gap-8 py-8 md:py-10 m-auto">
