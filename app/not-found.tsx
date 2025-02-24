baaf19e7a722fb927b65fab199f0f265255f7553
Dimash <dinmukhamed.amirov@gmail.com>
2025-02-24 11:28:28 +0500

    feat: add NotFound page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,18 @@
 export default function NotFound() {
   return (
-    <div>
-      <h1>Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-9xl font-bold text-error">404</h1>
+          <h2 className="text-3xl font-bold mt-4">Ой! Страница не найдена</h2>
+          <p className="py-6">
+            Извините, мы не можем найти страницу, которую вы ищете.
+          </p>
+          <a href="/" className="btn btn-neutral">
+            Вернуться на главную
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
