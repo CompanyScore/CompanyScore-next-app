9c5e33cc230ccaa38a9ddf368ffdf83b15396957
Dimash <dinmukhamed.amirov@gmail.com>
2025-03-05 17:02:46 +0500

    feat: add hero

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,26 @@ export default function HomePage() {
   // const [count, setCount] = useState();
   return (
     <section className="flex flex-col items-stretch justify-center gap-8 py-8 md:py-10 m-auto">
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+        }}
+      >
+        <div className="hero-overlay"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Хэй, привет!</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Оставить отзыв</button>
+          </div>
+        </div>
+      </div>
       <Title size="2">Честные отзывы о IT-компаниях!</Title>
       <Title size="2">Не трать время на тестовые зря!</Title>
       <Title size="2">Проверь компанию перед собеседованием!</Title>
