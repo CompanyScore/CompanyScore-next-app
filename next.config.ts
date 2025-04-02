2c91de856e9b722500f6341022e4da946b454868
Dimash <dinmukhamed.amirov@gmail.com>
2025-04-02 10:50:38 +0500

    feat: add docker compose

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,28 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["localhost", "companyscore.net", "api.companyscore.net"],
   },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       headers: [
+  //         {
+  //           key: "Access-Control-Allow-Origin",
+  //           value: "https://companyscore.net",
+  //         },
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         {
+  //           key: "Access-Control-Allow-Methods",
+  //           value: "GET,POST,PUT,DELETE,OPTIONS",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Headers",
+  //           value: "Content-Type, Authorization",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
