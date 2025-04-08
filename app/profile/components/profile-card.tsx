252a73681593b7224a84443640a0eb682af09c7c
Dimash <dinmukhamed.amirov@gmail.com>
2025-04-08 16:07:41 +0500

    fix: pics path r2

--- app/profile/components/profile-card.tsx
+++ app/profile/components/profile-card.tsx
@@ -40,7 +40,7 @@ export function ProfileCard() {
         </label>
         {profile?.avatar ? (
           <Image
-            src={`${process.env.NEXT_R2_IMAGES}${profile.avatar}`}
+            src={`${process.env.R2_IMAGES}${profile.avatar}`}
             alt="Avatar"
             width={400}
             height={400}
