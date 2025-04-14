2a3bc8b68cee3831dbe9527633e6986147e36fec
Dimash <dinmukhamed.amirov@gmail.com>
2025-04-14 16:21:14 +0500

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   productionBrowserSourceMaps: true,
   images: {
-    domains: ["localhost", "companyscore-images.ams3.digitaloceanspaces.com"],
+    domains: ["localhost", "companyscore-images.ams3.cdn.digitaloceanspaces.com"],
   },
   trailingSlash: false,
 };
