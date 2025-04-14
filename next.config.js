23a8dcb156a4ca4b4b9976f0a7f8a131e9a76118
Dimash <dinmukhamed.amirov@gmail.com>
2025-04-14 17:38:58 +0500

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   productionBrowserSourceMaps: true,
+  trailingSlash: false,
   images: {
     domains: [
       "localhost",
-      "digitaloceanspaces.com",
-      "ams3.digitaloceanspaces.com",
       "companyscore-images.ams3.digitaloceanspaces.com",
       "companyscore-images.ams3.cdn.digitaloceanspaces.com",
     ],
   },
-  trailingSlash: false,
 };
 
 export default nextConfig;
