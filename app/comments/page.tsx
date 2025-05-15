d286987d38a0a90414a7d04c30c594cc24f7f44b
Elena <110896958+Elena-web@users.noreply.github.com>
2025-05-15 10:29:27 +0300

    fix: link and button

--- app/comments/page.tsx
+++ app/comments/page.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 export default function CommentsPage() {
   return (
     <section className="flex flex-col items-stretch justify-center gap-8 py-8 md:py-10 m-auto">
-      <Button className="btn-secondary mr-0">
-        <Link href="/comments/comments-add">Оставить отзыв</Link>
-      </Button>
+        <Link href="/comments/comments-add" className="block w-[100%]" >
+			<Button className="btn-secondary mr-0 w-[100%]">Оставить отзыв</Button>
+		</Link>
       <div>Asd</div>
     </section>
   );
