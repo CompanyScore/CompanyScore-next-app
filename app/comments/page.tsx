5071812d1bea28d885b0eca6e8d3af359bcc5851
Elena <110896958+Elena-web@users.noreply.github.com>
2025-05-21 15:24:50 +0300

    feat:added CommentsTable

--- app/comments/page.tsx
+++ app/comments/page.tsx
@@ -1,5 +1,8 @@
 import { Button } from '@/ui';
 import Link from 'next/link';
+import { CommentsTable } from './comments-add/components/comments-add-table';
+import { CommentsShowBy } from './comments-add/components/comments-add-show-by';
+import { CommentsPagination } from './comments-add/components/comments-add-pagination';
 
 export default function CommentsPage() {
   return (
@@ -8,6 +11,11 @@ export default function CommentsPage() {
         <Link href="/comments/comments-add">Оставить отзыв</Link>
       </Button>
       <div>Asd</div>
+      <CommentsTable />
+      <div className="flex justify-between">
+        <CommentsShowBy />
+        <CommentsPagination />
+      </div>
     </section>
   );
 }
