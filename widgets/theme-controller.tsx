1a18893dd787f79c7051e64d781ac5832a2bbd6d
Dimash <dinmukhamed.amirov@gmail.com>
2025-06-26 17:54:25 +0500

    fix: http to https

--- widgets/theme-controller.tsx
+++ widgets/theme-controller.tsx
@@ -26,7 +26,7 @@ export function ThemeController() {
           width="12px"
           height="12px"
           className="h-2 w-2 fill-current opacity-60 inline-block"
-          xmlns="http://www.w3.org/2000/svg"
+          xmlns="https://www.w3.org/2000/svg"
           viewBox="0 0 2048 2048"
         >
           <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
