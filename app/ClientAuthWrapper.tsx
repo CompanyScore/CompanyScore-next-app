f114563176d4105b49b7999760d3caf4ecc826e5
Dimash <dinmukhamed.amirov@gmail.com>
2025-06-28 22:10:37 +0500

    fix: use localStorage for login

--- app/ClientAuthWrapper.tsx
+++ app/ClientAuthWrapper.tsx
@@ -1,37 +1,37 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { usePathname, useRouter } from 'next/navigation';
+// import { useEffect, useState } from 'react';
+// import { usePathname, useRouter } from 'next/navigation';
 
 export default function ClientAuthWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const router = useRouter();
-  const [isReady, setIsReady] = useState(false);
+  // const pathname = usePathname();
+  // const router = useRouter();
+  // const [isReady, setIsReady] = useState(false);
 
-  useEffect(() => {
-    const checkAuth = () => {
-      const accessToken = localStorage.getItem('accessToken');
-      const isLoginPage =
-        pathname === '/login' || pathname.startsWith('/login');
+  // useEffect(() => {
+  //   const checkAuth = () => {
+  //     const accessToken = localStorage.getItem('accessToken');
+  //     const isLoginPage =
+  //       pathname === '/login' || pathname.startsWith('/login');
 
-      if (!accessToken && !isLoginPage) {
-        router.replace('/login');
-      } else {
-        setIsReady(true);
-      }
-    };
+  //     if (!accessToken && !isLoginPage) {
+  //       router.replace('/login');
+  //     } else {
+  //       setIsReady(true);
+  //     }
+  //   };
 
-    // Проверяем после небольшого тайм-аута, чтобы дать браузеру прогрузиться
-    const timeout = setTimeout(checkAuth, 50);
+  //   // Проверяем после небольшого тайм-аута, чтобы дать браузеру прогрузиться
+  //   const timeout = setTimeout(checkAuth, 50);
 
-    return () => clearTimeout(timeout);
-  }, [pathname]);
+  //   return () => clearTimeout(timeout);
+  // }, [pathname]);
 
-  if (!isReady) return null;
+  // if (!isReady) return null;
 
   return <>{children}</>;
 }
