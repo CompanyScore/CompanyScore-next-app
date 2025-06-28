b2aacaf1a84b820f3257223ad8e6bad11e02e5c8
Dimash <dinmukhamed.amirov@gmail.com>
2025-06-28 22:00:28 +0500

    fix: use localStorage for login

--- app/ClientAuthWrapper.tsx
+++ app/ClientAuthWrapper.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { usePathname, useRouter } from 'next/navigation';
 
 export default function ClientAuthWrapper({
@@ -10,15 +10,22 @@ export default function ClientAuthWrapper({
 }) {
   const pathname = usePathname();
   const router = useRouter();
+  const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
     const accessToken = localStorage.getItem('accessToken');
 
-    // если мы не на /login и не залогинены — редиректим
-    if (pathname !== '/login' && !accessToken) {
+    const isLoginPage = pathname === '/login' || pathname.startsWith('/login');
+
+    if (!accessToken && !isLoginPage) {
       router.replace('/login');
+    } else {
+      setIsReady(true); // всё хорошо, показываем children
     }
   }, [pathname, router]);
+
+  // Пока не поняли, нужно ли редиректить — не рендерим
+  if (!isReady) return null;
 
   return <>{children}</>;
 }
