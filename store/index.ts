f896258b50cf4a51fab513520fb87117942089b8
Dimash <dinmukhamed.amirov@gmail.com>
2025-07-03 12:12:54 +0500

    delete: unused store files

--- store/index.ts
+++ store/index.ts
@@ -1,5 +1,3 @@
-export { useCommentFormStore } from './useCommentFormStore';
-export { useCommentFormStore2 } from './useCommentFormStore2';
 export { useCommentsStore } from './сomments';
 export { useProfileStore } from './profile';
 export { useCompaniesStore } from './companies';
