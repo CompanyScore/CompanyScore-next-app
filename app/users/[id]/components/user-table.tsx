79bbad51194743e2f5a769654e7f2cfc34a72546
Dimash <dinmukhamed.amirov@gmail.com>
2025-07-04 12:14:10 +0500

    fix: comment

--- app/users/[id]/components/user-table.tsx
+++ app/users/[id]/components/user-table.tsx
@@ -2,17 +2,16 @@
 
 import { useEffect } from 'react';
 
-import { redirect } from 'next/navigation';
+// import { redirect } from 'next/navigation';
 import { useCommentsStore } from '@/store';
-import moment from 'moment';
-import { ImageTable, Button, Tooltip, Title, Table } from '@/ui';
+// import moment from 'moment';
+import { Button, Title } from '@/ui';
 import Link from 'next/link';
 
 export type CommentType = {
   id: string;
   rating: number;
   createDate: Date;
-  text: string;
   position: string;
   company: {
     id: number;
@@ -45,50 +44,51 @@ export function UserTable() {
     );
   }
 
-  const columns = [
-    {
-      key: 'company',
-      title: 'Компания',
-      render: (comment: CommentType) => (
-        <div className="flex max-[650px]:justify-center items-center gap-4">
-          {comment.company.logo ? (
-            <ImageTable
-              className="max-[650px]:hidden"
-              src={process.env.NEXT_PUBLIC_BACK + comment.company.logo}
-            />
-          ) : (
-            <div className="skeleton h-32 w-32"></div>
-          )}
-          <p>{comment.company.name}</p>
-        </div>
-      ),
-    },
-    { key: 'position', title: 'Должность' },
-    { key: 'rating', title: 'Рейтинг' },
-    { key: 'text', title: 'Отзыв' },
-    {
-      key: 'createDate',
-      title: 'Дата',
-      render: (comment: CommentType) =>
-        moment(comment.createDate).format('MMM Do YY'),
-    },
-    {
-      key: 'actions',
-      title: 'Действия',
-      render: (comment: CommentType) => (
-        <div className="space-x-2">
-          <Tooltip tip="Посмотреть">
-            <Button
-              className="btn-neutral"
-              onClick={() => redirect(`/companies/${comment.company.id}`)}
-            >
-              <img src="/icons/file.svg" alt="File" width={25} height={25} />
-            </Button>
-          </Tooltip>
-        </div>
-      ),
-    },
-  ];
+  // const columns = [
+  //   {
+  //     key: 'company',
+  //     title: 'Компания',
+  //     render: (comment: CommentType) => (
+  //       <div className="flex max-[650px]:justify-center items-center gap-4">
+  //         {comment.company.logo ? (
+  //           <ImageTable
+  //             className="max-[650px]:hidden"
+  //             src={process.env.NEXT_PUBLIC_BACK + comment.company.logo}
+  //           />
+  //         ) : (
+  //           <div className="skeleton h-32 w-32"></div>
+  //         )}
+  //         <p>{comment.company.name}</p>
+  //       </div>
+  //     ),
+  //   },
+  //   { key: 'position', title: 'Должность' },
+  //   { key: 'rating', title: 'Рейтинг' },
+  //   { key: 'text', title: 'Отзыв' },
+  //   {
+  //     key: 'createDate',
+  //     title: 'Дата',
+  //     render: (comment: CommentType) =>
+  //       moment(comment.createDate).format('MMM Do YY'),
+  //   },
+  //   {
+  //     key: 'actions',
+  //     title: 'Действия',
+  //     render: (comment: CommentType) => (
+  //       <div className="space-x-2">
+  //         <Tooltip tip="Посмотреть">
+  //           <Button
+  //             className="btn-neutral"
+  //             onClick={() => redirect(`/companies/${comment.company.id}`)}
+  //           >
+  //             <img src="/icons/file.svg" alt="File" width={25} height={25} />
+  //           </Button>
+  //         </Tooltip>
+  //       </div>
+  //     ),
+  //   },
+  // ];
 
-  return <Table columns={columns} data={comments} />;
+  // return <Table columns={columns} data={comments} />;
+  return <div>asd</div>;
 }
