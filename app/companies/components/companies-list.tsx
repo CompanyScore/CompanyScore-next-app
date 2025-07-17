2daec9895e997a32775683c5b9b1cb1dd5d7d5e5
tearzday <biverdaniil@gmail.com>
2025-07-17 16:32:09 +0300

    feat: add infinity-list component

--- app/companies/components/companies-list.tsx
+++ app/companies/components/companies-list.tsx
@@ -1,49 +1,35 @@
 import { useCompanyStore } from '@/store/api';
 import { CompaniesCard } from './companies-card';
-import { useEffect, useRef } from 'react';
-import { Loading } from '@/ui';
+import InfinityList from '@/shared/infinity-list';
 
 export function CompaniesList() {
   const { companies, limit, total, page, getCompanies, loading } =
     useCompanyStore();
-  const lastElement = useRef<HTMLDivElement | null>(null);
-  const observer = useRef<IntersectionObserver | null>(null);
-
-  useEffect(() => {
-    if (observer.current) observer.current.disconnect();
-
-    if (!loading) {
-      const callback = function (entries: IntersectionObserverEntry[]) {
-        if (entries[0].isIntersecting && page < total / limit) {
-          getCompanies({ page: page + 1 });
-        }
-      };
-
-      observer.current = new IntersectionObserver(callback);
-      if (lastElement.current) {
-        observer.current.observe(lastElement.current);
-      }
-    }
-  }, [loading]);
 
   return (
-    <div className="flex gap-8 flex-wrap justify-center">
-      {companies.map(company => {
-        const { id, name, rating, logo, country, city } = company;
+    <div>
+      <InfinityList
+        limit={limit}
+        total={total}
+        page={page}
+        getNewElements={getCompanies}
+        loading={loading}
+      >
+        {companies.map(company => {
+          const { id, name, rating, logo, country, city } = company;
 
-        return (
-          <CompaniesCard
-            key={id}
-            name={name}
-            rating={rating}
-            logo={logo}
-            country={country}
-            city={city}
-          />
-        );
-      })}
-      {loading && <Loading />}
-      <div style={{ height: '20px' }} ref={lastElement} />
+          return (
+            <CompaniesCard
+              key={id}
+              name={name}
+              rating={rating}
+              logo={logo}
+              country={country}
+              city={city}
+            />
+          );
+        })}
+      </InfinityList>
     </div>
   );
 }
