2d2030ef343c0af40c68c84f4956f52ffb880ddc
tearzday <biverdaniil@gmail.com>
2025-08-01 17:35:12 +0300

    feat: add infinity-user-list mvp

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,17 +1,19 @@
 // app/users/page.tsx
 
-import { fetchUsersSSR } from '@/api/server/users';
-import { UsersFilter, UsersList } from './components';
+// import { fetchUsersSSR } from '@/api/server/users';
+import { UsersFilter } from './components';
+import InfinityTestList from './components/infinity-test-list';
 
 // export const revalidate = 60;
 
 export default async function UsersPage() {
-  const data = await fetchUsersSSR();
+  // const data = await fetchUsersSSR();
 
   return (
     <section className="flex flex-col gap-8 py-8 md:py-10 m-auto">
       <UsersFilter />
-      <UsersList users={data?.users || []} />
+      {/* <UsersList users={data?.users || []} /> */}
+      <InfinityTestList />
     </section>
   );
 }
