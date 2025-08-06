9d56467106ac7acc75f87acdb60174594e862ab9
Dimash <dinmukhamed.amirov@gmail.com>
2025-08-06 17:20:12 +0500

    fix: comment page

--- app/comments/components/index.tsx
+++ app/comments/components/index.tsx
@@ -1,3 +1,2 @@
-export { CommentsPagination } from './comments-pagination';
-export { CommentsShowBy } from './comments-show-by';
 export { CommentsTable } from './comments-table';
+export { FilterComments } from './filters.comments';
