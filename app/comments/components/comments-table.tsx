d07c3b267e30a494824c6d130e2af9837c92c8f4
Dimash <dinmukhamed.amirov@gmail.com>
2025-08-06 17:20:12 +0500

    fix: comment page

--- app/comments/components/comments-table.tsx
+++ app/comments/components/comments-table.tsx
@@ -5,8 +5,7 @@ import { useEffect } from 'react';
 import { redirect } from 'next/navigation';
 import { useCommentApi } from '@/store/api/comment.api';
 
-import { Button, ImageTable, Title, Card } from '@/shared/ui';
-import { IconTrash } from '@tabler/icons-react';
+import { ImageTable, Title, Card } from '@/shared/ui';
 import moment from 'moment';
 import Avatar, { genConfig } from 'react-nice-avatar';
 import {
@@ -14,10 +13,12 @@ import {
   adjectives,
   animals,
 } from 'unique-names-generator';
+
 import { FaStar } from 'react-icons/fa';
+import { IconHeart, IconDotsVertical } from '@tabler/icons-react';
 
 export function CommentsTable() {
-  const { comments, loading, getComments, deleteComment } = useCommentApi();
+  const { comments, loading, getComments } = useCommentApi();
 
   const config = genConfig();
   const nickname = uniqueNamesGenerator({
@@ -41,58 +42,72 @@ export function CommentsTable() {
   }
 
   return (
-    <>
+    <div className="flex flex-col gap-10 w-full">
       {comments.map(comment => (
         <Card
           key={comment.id}
-          className="flex flex-col gap-4 max-w-96"
+          className="flex flex-col gap-4 max-w-[912px] w-full"
           onClick={() => redirect(`/comments/${comment.id}`)}
         >
-          <div className="flex items-center gap-2">
-            <ImageTable
-              src={
-                comment.company?.logo
-                  ? `${process.env.NEXT_PUBLIC_S3_IMAGES}/${comment.company?.logo}`
-                  : '/imgs/company-logo.jpg'
-              }
-            />
-            {comment.company.name}
-          </div>
-
-          <div className="flex items-center gap-2">
-            {comment.user?.avatar ? (
-              <ImageTable
-                className="max-[650px]:hidden"
-                src={
-                  `${process.env.NEXT_PUBLIC_S3_IMAGES}/${comment.user.avatar}`
-
-                  // : '/imgs/avatar.jpg'
-                }
-              />
-            ) : (
-              <Avatar className="w-24 h-24" {...config} />
-            )}
-            <p>{comment.user?.name ?? nickname}</p>
-          </div>
-
-          <div className="flex gap-4">
-            {comment.score}
+          <div className="flex justify-between">
             <div className="flex items-center">
-              {comment.stars}
-              <FaStar className="text-yellow-400" />
+              {comment.stars} <FaStar className="text-yellow-400" /> |
+              {comment.score} баллов
+            </div>
+
+            <div className="flex gap-2">
+              <div>{moment(comment.createDate).format('MMM Do YY')}</div>
+              <IconDotsVertical stroke={2} />
             </div>
           </div>
 
-          <div className="flex flex-col">
-            <span>{comment.user.position.title}</span>
-            {moment(comment.createDate).format('MMM Do YY')}
+          <div className="flex justify-between">
+            <div className="flex items-center gap-2">
+              {comment.user?.avatar ? (
+                <ImageTable
+                  className="max-[650px]:hidden"
+                  src={`${process.env.NEXT_PUBLIC_S3_IMAGES}/${comment.user.avatar}`}
+                />
+              ) : (
+                <Avatar className="w-46 h-46" {...config} />
+              )}
+              <div className="flex flex-col">
+                <p>{comment.user?.name ?? nickname}</p>
+                <p>{comment.user?.position.title}</p>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-2">
+              {comment.company.name}
+              <ImageTable
+                src={
+                  comment.company?.logo
+                    ? `${process.env.NEXT_PUBLIC_S3_IMAGES}/${comment.company?.logo}`
+                    : '/imgs/company-logo.jpg'
+                }
+              />
+            </div>
           </div>
 
-          <Button onClick={() => deleteComment(comment.id)}>
-            <IconTrash stroke={2} className="text-red-600" />
-          </Button>
+          <div>Bage</div>
+
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+              <p className="text-neutral-500">Что понравилось</p>
+              <p>{comment.reasonJoined}</p>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <p className="text-neutral-500">Что не понравилось</p>
+              <p>{comment.reasonLeft}</p>
+            </div>
+          </div>
+
+          <div>
+            <IconHeart stroke={2} />
+          </div>
         </Card>
       ))}
-    </>
+    </div>
   );
 }
--- app/comments/page.tsx
+++ app/comments/page.tsx
@@ -1,23 +1,27 @@
-import { Button, Container, Title } from '@/shared/ui';
+import { Button, Container } from '@/shared/ui';
 import Link from 'next/link';
-import {
-  CommentsTable,
-  CommentsShowBy,
-  CommentsPagination,
-} from './components';
+import { CommentsTable, FilterComments } from './components';
 
 export default function CommentsPage() {
   return (
-    <Container className="flex flex-col gap-4">
-      <Title>Отзывы</Title>
-      <Link href="/comments/add" className="self-end">
+    <Container className="flex flex-col gap-20">
+      <div className="flex justify-between">
+        <p className="text-7xl font-bold">
+          Ваша история полезнее всех вакансий
+        </p>
+        <p>
+          Опыт реальных людей — лучшая навигация по рынку труда. Поделитесь
+          инсайтами и предостережениями, которые облегчат путь другим
+        </p>
+      </div>
+
+      <Link href="/comments/add">
         <Button>Оставить отзыв</Button>
       </Link>
 
-      <CommentsTable />
-      <div className="flex justify-between">
-        <CommentsShowBy />
-        <CommentsPagination />
+      <div className="flex gap-6">
+        <FilterComments />
+        <CommentsTable />
       </div>
     </Container>
   );
