bb417b609d512aa9772006820525025ce56e9c9c
Dimash <dinmukhamed.amirov@gmail.com>
2025-08-14 18:04:35 +0500

    refactor: fix header style

--- shared/ui/avatar.tsx
+++ shared/ui/avatar.tsx
@@ -6,7 +6,7 @@ type Props = {
 export function Avatar({ src, className }: Props) {
   return (
     <div className="avatar">
-      <div className={`ring-primary w-20 rounded-full ring-2 ${className}`}>
+      <div className={`ring-primary rounded-full ring-2 ${className}`}>
         <img src={src} alt="ImageTable" width={40} height={40} />
       </div>
     </div>
--- widgets/header.tsx
+++ widgets/header.tsx
@@ -3,8 +3,9 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React, { useEffect } from 'react';
 import { useProfileApi } from '@/store/api';
-// import { ThemeController } from './';
 import { Avatar } from '@/shared/ui';
+
+import { IconHeart, IconBell } from '@tabler/icons-react';
 
 export function Header() {
   const { profile, getProfile } = useProfileApi();
@@ -25,12 +26,12 @@ export function Header() {
   }, [getProfile]);
 
   return (
-    <div className="hidden min-[1300px]:flex justify-between items-center navbar bg-black text-white px-10">
-      <div className="max-w-96 w-full">
+    <div className="hidden min-[1300px]:flex justify-between items-center navbar bg-black text-white px-10 py-4">
+      <Link href="/" className="max-w-96 w-full cursor-pointer">
         <img src="/icons/header-logo.svg" alt="Logo" width={32} height={32} />
-        <h1 className="text-3xl">CompanyScore</h1>
-      </div>
-      <ul className="flex gap-10 text-2xl">
+        <h1 className="text-2xl">CompanyScore</h1>
+      </Link>
+      <ul className="flex gap-10 text-xl">
         {pages.map(({ href, label }) => (
           <li key={href}>
             <Link
@@ -44,10 +45,13 @@ export function Header() {
           </li>
         ))}
       </ul>
-      <div className="flex items-center justify-end gap-2 max-w-96 w-full">
+      <div className="flex items-center justify-end gap-4 max-w-96 w-full cursor-pointer">
+        <IconHeart stroke={1} />
+        <IconBell stroke={1} />
         <div className="dropdown dropdown-bottom dropdown-end">
-          <div tabIndex={0} role="button" className="">
+          <div tabIndex={0} role="button" className="flex items-center">
             <Avatar
+              className="w-12 h-12"
               src={
                 profile?.avatar
                   ? `${process.env.NEXT_PUBLIC_S3_IMAGES}/${profile.avatar}`
@@ -57,10 +61,11 @@ export function Header() {
           </div>
           <ul
             tabIndex={0}
-            className="dropdown-content menu bg-white text-black border border-gray-200 rounded-xl shadow-lg z-[9999] w-96 p-4 space-y-2"
+            className="dropdown-content menu bg-white text-black border border-gray-200 rounded-xl shadow-lg z-[9999] w-60 p-4 space-y-2"
           >
             <li className="flex flex-row items-center border-b pb-2">
               <Avatar
+                className="w-20 h-20"
                 src={
                   profile?.avatar
                     ? `${process.env.NEXT_PUBLIC_S3_IMAGES}/${profile.avatar}`
@@ -75,24 +80,23 @@ export function Header() {
             <li>
               <Link
                 href="/profile"
-                className="hover:bg-gray-100 rounded px-2 py-1"
+                className="hover:bg-gray-100 rounded px-2 py-1 cursor-pointer"
               >
                 Профиль
               </Link>
             </li>
             <li>
-              <a className="hover:bg-gray-100 rounded px-2 py-1">
+              <a className="hover:bg-gray-100 rounded px-2 py-1 cursor-pointer">
                 Пароль и безопасность
               </a>
             </li>
             <li>
-              <a className="text-red-500 hover:bg-red-50 rounded px-2 py-1">
+              <a className="text-red-500 hover:bg-red-50 rounded px-2 py-1 cursor-pointer">
                 Выйти
               </a>
             </li>
           </ul>
         </div>
-        {/* <ThemeController /> */}
       </div>
     </div>
   );
